875809ca78d285c3307bcd726523085e28d83953
carapaceonbear <seerhan17@gmail.com>
2022-07-21 12:46:29 +0100

    frontend, routing setup

--- courseology-frontend/src/App.jsx
+++ courseology-frontend/src/App.jsx
@@ -1,10 +1,23 @@
-import './App.scss';
+import "./App.scss";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Splash from "./containers/Splash/Splash";
+import BrowseCourses from "./containers/BrowseCourses/BrowseCourses";
+import CourseOverview from './containers/CourseOverview/CourseOverview';
+import SignIn from "./containers/SignIn/SignIn";
+import UserProfile from './containers/UserProfile/UserProfile';
 
 function App() {
   return (
-    <>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Splash />} />
+        <Route path="/browse" element={<BrowseCourses />} />
+        <Route path="/course/:courseId" element={<CourseOverview />} />
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/profile/:userId" element={<UserProfile />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
