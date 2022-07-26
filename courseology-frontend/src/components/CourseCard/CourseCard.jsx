ef644d800be78a5493f4e4acb33df3f1849da2f1
carapaceonbear <seerhan17@gmail.com>
2022-07-26 22:46:14 +0100

    frontend, course overview page

--- courseology-frontend/src/components/CourseCard/CourseCard.jsx
+++ courseology-frontend/src/components/CourseCard/CourseCard.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import "./CourseCard.scss";
+import { Link } from "react-router-dom";
 
 const CourseCard = ( {cardData} ) => {
 
-    const {course_name, subject, duration} = cardData;
+    const {id, course_name, subject, duration} = cardData;
 
   return (
-    <div className="card">
-        <h2 className="card__text card__text--name">{course_name}</h2>
-        <h3 className="card__text card__text--subject">{subject}</h3>
-        <h3 className="card__text card__text--duration">{duration}</h3>
-    </div>
+    <Link to={`/course/${id}`}>
+      <div className="card">
+          <h2 className="card__text card__text--name">{course_name}</h2>
+          <h3 className="card__text card__text--subject">{subject}</h3>
+          <h3 className="card__text card__text--duration">{duration}</h3>
+      </div>
+    </Link>
   )
 }
 
--- courseology-frontend/src/containers/CourseOverview/CourseOverview.jsx
+++ courseology-frontend/src/containers/CourseOverview/CourseOverview.jsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./CourseOverview.scss";
 import Navbar from "../../components/Navbar/Navbar";
+import { useParams } from "react-router-dom";
 
 const CourseOverview = () => {
+
+  const { courseId } = useParams();
+
+  const [courseData, setCourseData] = useState([]);
+
+  useEffect(() => {
+    getCourse(courseId).then(data => setCourseData(data))
+  }, [])
+
+  const getCourse = async (id) => {
+    let url = `http://localhost:8080/course/${id}`;
+    let request = await fetch(url);
+    let result = await request.json();
+    return result;
+  }
+
   return (
-    <div>
+    <div className="course">
       <Navbar />
+      <div className="course-container">
+        <div className="course-details">
+          <h1 className="course-details__name">{courseData.course_name}</h1>
+          <h2 className="course-details__id">Course ID: {courseData.id}</h2>
+          <h3 className="course-details__info course-details__info--subject">{courseData.subject}</h3>
+          <h3 className="course-details__info course-details__info--duration">Course duration - {courseData.duration}</h3>
+          <h3 className="course-details__info course-details__info--price">Fees: £{courseData.price}</h3>
+          <h3 className="course-details__info course-details__info--tutor">Course tutor: {courseData.tutor}</h3>
+        </div>
+      </div>
     </div>
   )
 }
